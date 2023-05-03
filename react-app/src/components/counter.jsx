b00f5282bd6eb7d12273c79df2fab3126de3f403
Kateryna Kuzmenko <katerinakuzmenko03@gmail.com>
2023-05-03 19:01:07 +0200

    projekt

---
+++ react-app/src/components/counter.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        count: 0
+    };
+
+    incrementNumber = () => {
+        this.setState({count: this.state.count + 1});
+    };
+
+    currentCount = () => {
+        return this.state.count === 0 ? 'Zero' : this.state.count;
+    };
+
+    resetCount = () => {
+        this.setState({count: 0
+        });
+        //"badge bg-primary"
+    };
+
+
+
+    render() {
+        return <div>
+            <span slassName="badge badge-primary m-2">{this.currentCount()}</span>
+            <button className="btn btn-secondary btn-sm" onClick={this.incrementNumber}>Add value</button>
+            <button className="btn btn-danger btn-sm" onClick={this.resetCount}>Reset</button>
+
+        </div>
+    }
+}
+
+
+
+
+
+
+
+export default Counter;
